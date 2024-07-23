c03ab721361a5d5aede84532bfafcd1bbb184bd8
Yash Sisodia <121327020+yash-sis@users.noreply.github.com>
2024-07-23 12:22:25 +0530

    Update 4_dijkstra.c

--- adalab/4_dijkstra.c
+++ adalab/4_dijkstra.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
-#include <time.h>
 #define INF 999
 
-int d[20], s[20], w[20][20], p[20], n, v;
+void dijkstra(int a[20][20], int n, int s) {
+    int p[20], d[20], v[20] = {0}, min, u;
 
-int findmin() {
-    int min = -1;
-
-    for (int i = 1; i <= n; i++) 
-        if (s[i] == 0 && (min == -1 || d[i] < d[min])) 
-            min = i;
-
-    return min;
-}
-
-void dijkstra() {
     for (int i = 1; i <= n; i++) {
-        s[i] = 0;
-        d[i] = INF;
-        p[i] = 0;
+        p[i] = s;
+        d[i] = a[s][i];
     }
 
-    d[v] = 0;
+    v[s] = 1;
+    d[s] = 0;
 
-    for (int k = 1; k <= n; k++) { 
-        int u = findmin();
+    for (int i = 1; i < n; i++) {
+        min = INF;
 
-        s[u] = 1;
+        for (int j = 1; j <= n; j++) {
+            if (v[j] == 0 && d[j] < min) {
+                min = d[j];
+                u = j;
+            }
+        }
 
-        for (int w1 = 1; w1 <= n; w1++) { //w1 is neighbour
-            if (w[u][w1] != INF && s[w1] == 0 && d[w1] > d[u] + w[u][w1]) {
-                d[w1] = d[u] + w[u][w1];
-                p[w1] = u;
+        v[u] = 1;
+
+        for (int j = 1; j <= n; j++) {
+            if (v[j] == 0 && d[u] + a[u][j] < d[j]) {
+                d[j] = d[u] + a[u][j];
+                p[j] = u;
             }
         }
     }
 
-    printf("Shortest path costs from vertex %d:\n", v);
+    printf("Shortest path costs from vertex %d:\n", s);
     for (int i = 1; i <= n; i++) {
         if (d[i] == INF)
             printf("No path to vertex %d\n", i);
@@ -46,51 +42,51 @@ void dijkstra() {
 
     printf("Shortest group of paths are:\n");
     for (int i = 1; i <= n; i++) {
-        if (i != v && d[i] != INF) {
+        if (i != s && d[i] != INF) {
             printf("%d", i);
 
             int j = p[i];
 
-            while (p[j] != 0) {
+            while (j != s) {
                 printf(" <- %d", j);
+
                 j = p[j];
             }
 
-            printf(" <- %d\n", v);
+            printf(" <- %d\n", s);
         }
     }
 }
 
 int main() {
-    printf("Enter the number of vertices: ");
+    int a[20][20], n, s;
+
+    printf("Enter the number of nodes: ");
     scanf("%d", &n);
 
     printf("Enter the cost adjacency matrix:\n");
-    for (int i = 1; i <= n; i++) 
-        for (int j = 1; j <= n; j++) 
-            scanf("%d", &w[i][j]);
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            scanf("%d", &a[i][j]);
 
-    printf("Enter the source vertex: ");
-    scanf("%d", &v);
+    printf("\nEnter the source node: ");
+    scanf("%d", &s);
 
-    clock_t start = clock();
-    dijkstra();
-    clock_t end = clock();
-
-    printf("Time taken: %lf s\n", (double)(end - start) / CLOCKS_PER_SEC);
+    dijkstra(a, n, s);
 
     return 0;
 }
 
 /*
-Enter the number of vertices: 5
+Enter the number of nodes: 5
 Enter the cost adjacency matrix:
 0 3 999 7 999
 999 0 4 2 999
 999 4 0 5 6
 7 2 5 0 4
 999 999 6 4 0
-Enter the source vertex: 1
+
+Enter the source node: 1
 Shortest path costs from vertex 1:
 To 1: 0
 To 2: 3
@@ -102,5 +98,5 @@ Shortest group of paths are:
 3 <- 2 <- 1
 4 <- 2 <- 1
 5 <- 4 <- 2 <- 1
-Time taken: 0.013000 s
 */
+
