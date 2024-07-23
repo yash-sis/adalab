764fc73563c916d07bf5c9574366e2bbc196d065
Yash Sisodia <121327020+yash-sis@users.noreply.github.com>
2024-07-23 12:21:57 +0530

    Update 3b_warshall.c

--- adalab/3b_warshall.c
+++ adalab/3b_warshall.c
@@ -1,39 +1,30 @@
 #include <stdio.h>
-#include <time.h>
 
-void warshall(int n, int a[20][20], int p[20][20]) {
-    for (int i = 0; i < n; i++) 
-        for (int j = 0; j < n; j++)
-            p[i][j] = a[i][j];
-    
+void warshall(int a[20][20], int n) {    
     for (int k = 0; k < n; k++) 
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                if (p[i][k] && p[k][j])
-                    p[i][j] = 1;
+                if (a[i][k] && a[k][j])
+                    a[i][j] = 1;
+
+    printf("\nThe transitive closure is:\n");
+    for (int i = 0; i < n; i++, printf("\n")) 
+        for (int j = 0; j < n; j++)
+            printf("%d\t", a[i][j]);
 }
 
 int main() {
-    int a[20][20], p[20][20], n;
+    int a[20][20], n;
 
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
 
-    printf("Enter the cost adjacency matrix:\n");
+    printf("Enter the adjacency matrix:\n");
     for (int i = 0; i < n; i++) 
         for (int j = 0; j < n; j++)
             scanf("%d", &a[i][j]);
 
-    clock_t start = clock();
-    warshall(n, a, p);
-    clock_t end = clock();
-
-    printf("The transitive closure is:\n");
-    for (int i = 0; i < n; i++, printf("\n")) 
-        for (int j = 0; j < n; j++)
-            printf("%d\t", p[i][j]);
-
-    printf("\nTime taken: %f s\n", (double)(end - start) / CLOCKS_PER_SEC);
+    warshall(a, n);
 
     return 0;
 }
@@ -45,11 +36,10 @@ Enter the adjacency matrix:
 0 0 0 1 
 0 0 0 0 
 1 0 1 0
+
 The transitive closure is:
 1       1       1       1
 1       1       1       1
 0       0       0       0
 1       1       1       1
-
-Time taken: 0.000000 s
 */
