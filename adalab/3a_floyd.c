53d611d7db6b33ed04bf3ed338dbb9707c7e2f34
Yash Sisodia <121327020+yash-sis@users.noreply.github.com>
2024-07-23 12:21:23 +0530

    Update 3a_floyd.c

--- adalab/3a_floyd.c
+++ adalab/3a_floyd.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
-#include <time.h>
-#define INF 999
 
-void floyd(int graph[20][20], int n) {
-    int dist[20][20];
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            dist[i][j] = graph[i][j];
-
+void floyd(int a[20][20], int n) {
     for (int k = 0; k < n; k++) 
         for (int i = 0; i < n; i++) 
             for (int j = 0; j < n; j++) 
-                if (dist[i][k] + dist[k][j] < dist[i][j])
-                    dist[i][j] = dist[i][k] + dist[k][j];
+                if (a[i][k] + a[k][j] < a[i][j])
+                    a[i][j] = a[i][k] + a[k][j];
 
     printf("\nThe resultant shortest path matrix is:\n");
-    for (int i = 0; i < n; i++, printf("\n")) {
-        for (int j = 0; j < n; j++) {
-            if (dist[i][j] == INF)
-                printf("%4s", "INF");
-            else
-                printf("%4d", dist[i][j]);
-        }
-    }
+    for (int i = 0; i < n; i++, printf("\n")) 
+        for (int j = 0; j < n; j++) 
+                printf("%4d", a[i][j]);
 }
 
 int main() {
-    int graph[20][20], n;
+    int a[20][20], n;
     
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
@@ -35,13 +22,9 @@ int main() {
     printf("Enter the cost adjacency matrix:\n");
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++) 
-            scanf("%d", &graph[i][j]);
+            scanf("%d", &a[i][j]);
 
-    clock_t start = clock();
-    floyd(graph, n);
-    clock_t end = clock();
-
-    printf("\nTime taken: %f s\n", (double)(end - start) / CLOCKS_PER_SEC);
+    floyd(a, n);
 
     return 0;
 }
@@ -59,6 +42,4 @@ The resultant shortest path matrix is:
    2   0   5   6
    7   7   0   1
    6  16   9   0
-
-Time taken: 0.010000 s 
 */
